2fca0c75cf704ee763fc8cc2efa4c4d70e72f5bb
Flavio Souza <fjms@cin.ufpe.br>
2022-06-27 15:48:24 -0300

    separando layout com .kv

--- main.py
+++ main.py
@@ -1,28 +1,11 @@
 from kivy.app import App
-from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
 
+class Contador(BoxLayout):
+    pass
 
-class Test(App):
+class Test(App):         #criar test.kv
     def build(self):
-        box1 = BoxLayout()
-        box2 = BoxLayout(orientation="vertical")
-        button1 = Button(text="+", font_size=30, on_release=self.incrementar)
-        button2 = Button(text="-", font_size=30, on_release=self.decrementar)
-        self.label = Label(text="0", font_size=30)
-        box1.add_widget(button1)
-        box1.add_widget(button2)
-        box2.add_widget(box1)
-        box2.add_widget(self.label)
-
-        return box2
-
-    def incrementar(self, button1):
-        self.label.text = str(int(self.label.text) + 1)
-
-    def decrementar(self, button2):
-        self.label.text = str(int(self.label.text) - 1)
-
+         return Contador()
 
 Test().run()
